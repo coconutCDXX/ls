4ad6c2b58d5152ed814dde06dc9264bb6c783dab
coralie <scbennard@gmail.com>
2017-12-13 01:23:23 -0800

    more functions, struct passing, lots to do -R mainly and printing

--- inspectfile.c
+++ inspectfile.c
@@ -9,40 +9,40 @@
 
 int main (int ac, char **av)
 {
-	struct stat info;
+	struct stat stats;
 	struct passwd *person;
 	struct group *grp;
-	//info = malloc(sizeof(struct stat));
+	//stats = malloc(sizeof(struct stat));
 
 	if (ac == 1)
 	{
 		perror("File Open");
 		exit(EXIT_FAILURE);
 	}
-	stat(av[1], &info);
-	person = getpwuid(info.st_uid);
-	grp = getgrgid(info.st_gid);
+	stat(av[1], &stats);
+	person = getpwuid(stats.st_uid);
+	grp = getgrgid(stats.st_gid);
 
 	printf("Name: %s\n", av[1]);
 
 	printf("Type: ");
-	printf( (S_ISBLK(info.st_mode)) ? "Block" : (S_ISCHR(info.st_mode)) ? "Character" :
-			(S_ISDIR(info.st_mode)) ? "Directory" : (S_ISFIFO(info.st_mode)) ? "Pipe/Fifo" :
-			(S_ISREG(info.st_mode)) ? "File" : (S_ISLNK(info.st_mode)) ? "Symbolic link" : "Error");
+	printf( (S_ISBLK(stats.st_mode)) ? "Block" : (S_ISCHR(stats.st_mode)) ? "Character" :
+			(S_ISDIR(stats.st_mode)) ? "Directory" : (S_ISFIFO(stats.st_mode)) ? "Pipe/Fifo" :
+			(S_ISREG(stats.st_mode)) ? "File" : (S_ISLNK(stats.st_mode)) ? "Symbolic link" : "Error");
 	printf("\nModes: ");
-	printf( (S_ISDIR(info.st_mode)) ? "d" : "-");
-	printf( (info.st_mode & S_IRUSR) ? "r" : "-");
-	printf( (info.st_mode & S_IWUSR) ? "w" : "-");
-	printf( (info.st_mode & S_IXUSR) ? "x" : "-");
-	printf( (info.st_mode & S_IRGRP) ? "r" : "-");
-	printf( (info.st_mode & S_IWGRP) ? "w" : "-");
-	printf( (info.st_mode & S_IXGRP) ? "x" : "-");
-	printf( (info.st_mode & S_IROTH) ? "r" : "-");
-	printf( (info.st_mode & S_IWOTH) ? "w" : "-");
-	printf( (info.st_mode & S_IXOTH) ? "x" : "-");
-	printf("\nNumber of links: %hu\n", info.st_nlink);
+	printf( (S_ISDIR(stats.st_mode)) ? "d" : "-");
+	printf( (stats.st_mode & S_IRUSR) ? "r" : "-");
+	printf( (stats.st_mode & S_IWUSR) ? "w" : "-");
+	printf( (stats.st_mode & S_IXUSR) ? "x" : "-");
+	printf( (stats.st_mode & S_IRGRP) ? "r" : "-");
+	printf( (stats.st_mode & S_IWGRP) ? "w" : "-");
+	printf( (stats.st_mode & S_IXGRP) ? "x" : "-");
+	printf( (stats.st_mode & S_IROTH) ? "r" : "-");
+	printf( (stats.st_mode & S_IWOTH) ? "w" : "-");
+	printf( (stats.st_mode & S_IXOTH) ? "x" : "-");
+	printf("\nNumber of links: %hu\n", stats.st_nlink);
 	printf("Owner: %s\n", person->pw_name);
 	printf("Group: %s\n", grp->gr_name);
-	printf("Size: %lld\n", info.st_size);
-	printf("Last modification date: %ld : %s\n", info.st_atime, ctime(&info.st_atime));
+	printf("Size: %lld\n", stats.st_size);
+	printf("Last modification date: %ld : %s\n", stats.st_atime, ctime(&stats.st_atime));
 }
--- main.c
+++ main.c
@@ -36,11 +36,11 @@ int		main(int ac, char **av)
 
 void	read_options(int ac, char **av, char *options)
 {
-	t_opt	*struct_opt;
-	t_info	*struct_info;
+	t_opt	struct_opt;
+	t_info	struct_info;
 
-	struct_info = (t_info*)malloc(sizeof(*struct_info));
-	struct_opt = (t_opt*)malloc(sizeof(*struct_opt));
+	//struct_info = (t_info)malloc(sizeof(struct_info));
+	//struct_opt = (t_opt)malloc(sizeof(struct_opt));
 	if (ft_strchr(options, 'a'))
 	struct_opt.opt_a = TRUE;
 	if (ft_strchr(options, 'l'))
@@ -51,7 +51,7 @@ void	read_options(int ac, char **av, char *options)
 	struct_opt.opt_r = TRUE;
 	if (ft_strchr(options, 't'))
 	struct_opt.opt_t = TRUE;
-	save_data1(struct_info);
+	save_data1(&struct_info);
 	if (ac == 2)
 	{
 		printf("do the options on the current DIR\n");
@@ -64,49 +64,139 @@ void	read_options(int ac, char **av, char *options)
 void		save_data1(t_info *sinfo)
 {
 	int dir_counter;
-	//struct stat stats;
-	//struct passwd *person;
-	//struct group *grp;
+	struct stat stats;
 	struct dirent *read;
 	DIR *p;
-	//char *filename;
-	//int	l;
 
 	dir_counter = count_dir();
+	(*sinfo).dir_cont = dir_counter;
 	p = opendir("./");
-	if ((read = readdir(p)) != NULL)
+	while ((read = readdir(p)) != NULL)
 	{
-		set_types(&sinfo, read->d_name);
-		set_rights(&sinfo, read->d_name);
+		set_types_name(sinfo, read->d_name);
+		set_rights(sinfo, read->d_name);
+		set_uid_gid_size(sinfo, read->d_name);
+		set_time(sinfo, read->d_name);
+		stat(read->d_name, &stats);
+		if (S_ISDIR(stats.st_mode))
+		{
+			(*sinfo)->tree = (s_info)malloc(sizeof(sinfo));
+			save_data2(sinfo, read->d_name);
+		}
 	}
-		// l = strlen(read->d_name);
-		// filename = (char*)malloc(sizeof(char) * l + 1);
-		// strcpy(d_name, filename);
 }
 
-void		set_types(t_info **sinfo, char *filename)
+void		set_types_name(t_info *sinfo, char *filename)
+{
+	struct stat stats;
+	int l;
+
+	l = strlen(filename);
+	(*sinfo)->filename = (char*)malloc(sizeof(char) * l + 1);
+	strcpy((*sinfo)->filename, filename);
+	stat(filename, &stats);
+	if (S_ISBLK(stats.st_mode))
+		(*sinfo).file_type = 3;
+	if (S_ISCHR(stats.st_mode))
+		(*sinfo).file_type = 4;
+	if (S_ISDIR(stats.st_mode))
+		(*sinfo).file_type = 2;
+	if (S_ISFIFO(stats.st_mode))
+		(*sinfo).file_type = 5;
+	if (S_ISREG(stats.st_mode))
+		(*sinfo).file_type = 1;
+	if (S_ISLNK(stats.st_mode))
+		(*sinfo).file_type = 6;
+}
+
+void		set_rights(t_info *sinfo, char *filename)
 {
 	struct stat stats;
 
 	stat(filename, &stats);
-	if (S_ISBLK(stats.st_mode))
-		*sinfo.file_type = 3;
-	if (S_ISCHR(stats.st_mode))
-		*sinfo.file_type = 4;
-	if (S_ISDIR(stats.st_mode))
-		*sinfo.file_type = 2;
-	if (S_ISFIFO(stats.st_mode))
-		*sinfo.file_type = 5;
-	if (S_ISREG(stats.st_mode))
-		*sinfo.file_type = 1;
-	if (S_ISLNK(stats.st_mode))
-		*sinfo.file_type = 6;
+	(*sinfo)->str_rights = (char*)malloc(sizeof(char) * 11 + 1);
+	switch((*sinfo).file_type)
+	{
+		case 1 :
+		(*sinfo)->str_rights[0] = '-';
+		case 2 :
+		(*sinfo)->str_rights[0] = 'D';
+		case 3 :
+		(*sinfo)->str_rights[0] = 'B';
+		case 4 :
+		(*sinfo)->str_rights[0] = 'C';
+		case 5 :
+		(*sinfo)->str_rights[0] = 'F';
+		case 6 :
+		(*sinfo)->str_rights[0] = 'L';
+	}
+	set_rightsbis(sinfo, stats);
 }
 
-void		set_rights(t_info **sinfo, char *filename)
+void		set_rights_USR_GRP(t_info *sinfo, struct stats)
 {
-	 
+	if (stats.st_mode & S_IRUSR)
+		(*sinfo)->str_rights[1] = 'r';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IWUSR)
+		(*sinfo)->str_rights[1] = 'w';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IXUSR)
+		(*sinfo)->str_rights[1] = 'x';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IRGRP)
+		(*sinfo)->str_rights[1] = 'r';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IWGRP)
+		(*sinfo)->str_rights[1] = 'w';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IXGRP)
+		(*sinfo)->str_rights[1] = 'x';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	set_rightstris(sinfo, stats);
 }
+
+void		set_rights_OTH(t_info *sinfo, struct stats)
+{
+	if (stats.st_mode & S_IROTH)
+		(*sinfo)->str_rights[1] = 'r';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IWOTH)
+		(*sinfo)->str_rights[1] = 'w';
+	else
+		(*sinfo)->str_rights[1] = '-';
+	if (stats.st_mode & S_IXOTH)
+		(*sinfo)->str_rights[1] = 'x';
+	else
+		(*sinfo)->str_rights[1] = '-';
+}
+
+void		set_uid_gid_size(t_info *sinfo, char *filename)
+{
+	struct stat stats;
+	struct passwd *person;
+	struct group *grp;
+	int l;
+
+	stat(filename, &stats);
+	person = getpwuid(stats.st_uid);
+	l = strlen(person->pw_name);
+	(*sinfo)->user_name = (char*)malloc(sizeof(char) * l + 1);
+	strcpy((*sinfo)->user_name, person->pw_name);
+	grp = getgrpid(stats.st_gid);
+	l = strlen(grp->gr_name);
+	(*sinfo)->grp_name = (char*)malloc(sizeof(char) * l + 1);
+	strcpy((*sinfo)->grp_name, grp->gr_name);
+	(*sinfo).bytes = stats.st_size;
+}
+
 int		count_dir(void)
 {
 	DIR *p;
@@ -150,7 +240,7 @@ int		verify_options(char *opt, char *ret)
 
 	i = 0;
 	if (opt[0] != '-')
-		return 2;
+		return 2;r--
 	opt++;
 	while (*opt != '\0')
 	{
